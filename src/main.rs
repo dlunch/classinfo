505b6e39ba90f806c15c25b9a183d2c4c1648345
Inseok Lee <git@dlun.ch>
2021-12-26 10:33:28 +0900

    Retrieve filename from commandline parameter

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,24 @@ mod class;
 mod vtable;
 
 use anyhow::Result;
+use clap::Parser;
+use tokio::fs;
+
+#[derive(Parser)]
+struct Args {
+    file_name: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let _ = pretty_env_logger::init();
+
+    let args = Args::parse();
+
+    let file = fs::read(args.file_name).await?;
+    let obj = object::File::parse(&*file)?;
+
+    vtable::find_vtables(&obj)?;
 
     Ok(())
 }
