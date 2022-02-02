92170c529683f83207fb8fb457b3776ef7f5f34c
Inseok Lee <git@dlun.ch>
2022-02-02 12:57:45 +0900

    Parallel disassembly

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,8 +1,12 @@
+use std::iter;
+
 use anyhow::{anyhow, Result};
 use capstone::{
     arch::{x86, BuildsCapstone, DetailsArchInsn},
     Capstone,
 };
+use memchr::memmem::Finder;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 pub struct Instruction {
     pub address: u64,
@@ -11,7 +15,7 @@ pub struct Instruction {
     pub operands: Vec<x86::X86Operand>,
 }
 
-pub fn disassemble_all(code: &[u8], addr: u64, pointer_size: usize) -> Result<Vec<Instruction>> {
+fn disassemble(code: &[u8], addr: u64, pointer_size: usize) -> Result<Vec<Instruction>> {
     let mut cs = Capstone::new()
         .x86()
         .mode(if pointer_size == 4 {
@@ -47,4 +51,55 @@ pub fn disassemble_all(code: &[u8], addr: u64, pointer_size: usize) -> Result<Ve
             })
         })
         .collect::<Vec<_>>())
+}
+
+pub fn disassemble_all(code: &[u8], addr: u64, pointer_size: usize) -> Result<Vec<Instruction>> {
+    let cpus = rayon::current_num_threads();
+    let step = code.len() / cpus;
+
+    if step < 0x10000 {
+        return disassemble(code, addr, pointer_size);
+    }
+
+    let int3_finder = Finder::new(b"\xcc\xcc\xcc\xcc\xcc");
+    let nop_finder = Finder::new(b"\x90\x90\x90\x90\x90");
+    let zero_finder = Finder::new(b"\x00\x00\x00\x00\x00");
+
+    let split = (0..code.len())
+        .step_by(step)
+        .map(|x| {
+            if x == 0 {
+                x
+            } else if x + step > code.len() {
+                code.len()
+            } else {
+                let offset = int3_finder
+                    .find(&code[x..])
+                    .or_else(|| nop_finder.find(&code[x..]))
+                    .or_else(|| zero_finder.find(&code[x..]))
+                    .unwrap();
+                x + offset
+            }
+        })
+        .chain(iter::once(code.len()))
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter_map(|x| (x[1] != x[0]).then(|| x[0]..x[1]))
+        .collect::<Vec<_>>();
+
+    let result = split
+        .into_par_iter()
+        .map(|x| {
+            let start = x.start;
+            let end = x.end;
+
+            log::trace!("disassemble: {}..{} {:?}", start, end, std::thread::current().id());
+            let result = disassemble(&code[x], addr + start as u64, pointer_size);
+            log::trace!("disassemble done: {}..{}", start, end);
+
+            result
+        })
+        .collect::<Result<Vec<_>, _>>()?; // do we have to collect twice?
+
+    Ok(result.into_iter().flatten().collect())
 }
